454836bb30dd0ca215d9e9e605a2e9d8be815ab6
Leon Radley <leon@digiplant.se>
2012-06-27 08:11:18 +0200

    v1.0.6

--- src/main/g8/$application_name$/app/Global.scala
+++ src/main/g8/$application_name$/app/Global.scala
@@ -6,6 +6,14 @@ import se.radley.plugin.salat._
 object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
+  	/*if (User.count(DBObject(), Nil, Nil) == 0) {
+      Logger.info("Loading Testdata")
+
+      User.save(User(
+        username = "leon",
+        password = "1234"
+      ))
+    }*/
   }
 
   override def onStop(app: Application) {
--- src/main/g8/$application_name$/app/models/User.scala
+++ src/main/g8/$application_name$/app/models/User.scala
@@ -6,6 +6,7 @@ import com.novus.salat._
 import com.novus.salat.dao._
 import com.mongodb.casbah.Imports._
 import se.radley.plugin.salat._
+import salatcontext._
 
 case class User(
   id: ObjectId = new ObjectId,
