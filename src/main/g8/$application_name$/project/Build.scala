103e6dcdde71fd020f998f596ebcffd99e4e7f0b
Kouichi Akatsuka <akatsuka@dwango.co.jp>
2012-10-10 11:40:50 +0900

    add scalariform

--- src/main/g8/$application_name$/project/Build.scala
+++ src/main/g8/$application_name$/project/Build.scala
@@ -1,11 +1,15 @@
 import sbt._
 import Keys._
 import PlayProject._
+import com.typesafe.sbtscalariform.ScalariformPlugin._
+import scalariform.formatter.preferences._
 
 object ApplicationBuild extends Build {
 
     val appName         = "$application_name$"
     val appVersion      = "1.0"
+
+    lazy val riformKeys = ScalariformKeys.preferences := FormattingPreferences().setPreference(AlignSingleLineCaseStatements, true)
 
     val appDependencies = Seq(
       "se.radley" %% "play-plugins-salat" % "$salat_plugin_version$"
@@ -15,6 +19,6 @@ object ApplicationBuild extends Build {
       routesImport += "se.radley.plugin.salat.Binders._",
       templatesImport += "org.bson.types.ObjectId",
       resolvers += "Sonatype" at "https://oss.sonatype.org/content/repositories/snapshots"
-    )
+    ).settings(scalariformSettings: _*).settings(riformKeys)
 
 }
--- src/main/g8/$application_name$/project/plugins.sbt
+++ src/main/g8/$application_name$/project/plugins.sbt
@@ -6,3 +6,8 @@ resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/release
 
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("play" % "sbt-plugin" % "$play_version$")
+
+// Use scalaRiform
+//addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "latest.integration")
+addSbtPlugin("com.typesafe.sbtscalariform" % "sbtscalariform" % "0.4.0")
+
